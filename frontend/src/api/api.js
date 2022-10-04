28b7c8bce07198f102adea7671aa64ebe002855b
Maria Korchagina <kobysheva_maria@mail.ru>
2022-10-04 13:25:37 +0300

    check linter errors

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,22 +3,45 @@ import { actions as messagesActions } from '../slices/messages.js';
 import { actions as channelsActions } from '../slices/channels.js';
 
 const buildtSocketApi = (socket) => {
-  const addNewMessage = (message) => socket.emit('newMessage', message)
+  const addNewMessage = (message) => socket.emit('newMessage', message, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
   socket.on('newMessage', (payload) => {
     store.dispatch(messagesActions.addMessage(payload));
   });
 
-  const addNewChannel = (channel) => socket.emit('newChannel', channel)
+  const addNewChannel = (channel) => socket.emit('newChannel', channel, (response) => {
+    if (response.status === 'ok') {
+      const { id } = response.data;
+      store.dispatch(channelsActions.setCurrentChannelId(id));
+    } else {
+      console.log(response.status);
+    }
+  });
+
   socket.on('newChannel', (payload) => {
     store.dispatch(channelsActions.addChannel(payload));
   });
 
-  const removeChannel = (id) => socket.emit('removeChannel', { id })
+  const removeChannel = (id) => socket.emit('removeChannel', { id }, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
   socket.on('removeChannel', (payload) => {
     store.dispatch(channelsActions.removeChannel(payload));
   });
 
-  const renameChannel = (renamedChannel) => socket.emit('renameChannel', renamedChannel)
+  const renameChannel = (renamedChannel) => socket.emit('renameChannel', renamedChannel, (response) => {
+    if (response.status !== 'ok') {
+      console.log(response.status);
+    }
+  });
+
   socket.on('renameChannel', (payload) => {
     const { name, id } = payload;
     store.dispatch(channelsActions.changeChannelName({
--- frontend/src/slices/selectors.js
+++ frontend/src/slices/selectors.js
@@ -2,18 +2,12 @@ import { selectors as channelsSelectors } from './channels.js';
 import { selectors as messagesSelectors } from './messages.js';
 
 export const getAllChannels = (state) => channelsSelectors.selectAll(state);
-
 export const getCurrentChannelId = (state) => state.channels.currentChannelId;
-
 export const getCurrentChannel = (state) => {
   const currentChannelId = getCurrentChannelId(state);
   return channelsSelectors.selectById(state, currentChannelId);
 };
-
 export const isDataFetching = (state) => state.channels.isFetching;
-
 export const getAllMessages = (state) => messagesSelectors.selectAll(state);
-
 export const getModalType = (state) => state.modals.modalType;
-
 export const getItemId = (state) => state.modals.itemId;
