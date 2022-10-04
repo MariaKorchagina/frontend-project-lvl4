b28514013058910f6656ceb4d3c2439d8b0196a7
Maria Korchagina <kobysheva_maria@mail.ru>
2022-10-04 12:52:22 +0300

    check linter errors

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
 export const getAuthHeader = () => {
   const currentUser = JSON.parse(localStorage.getItem('user'));
   if (currentUser && currentUser.token) {
